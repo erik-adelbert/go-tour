111105a38447a4947fe3049d581292192416abc2
erik-adelbert <42875866+erik-adelbert@users.noreply.github.com>
2018-09-02 19:27:14 +0200

    Update exercise-errors.go

--- exercise-errors.go
+++ exercise-errors.go
@@ -17,15 +17,14 @@ func (e ErrNegativeSqrt) Error() string {
 	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
 }
 
-const ε = 1e-10
-
 func Sqrt(x float64) (float64, error) {
 	if x < 0 {
 		return 0, ErrNegativeSqrt(x)
 	}
 
-	r, δ := x, float64(1)
-	for math.Abs(δ) > ε {
+	const ε = 1e-10
+	r := float64(1)
+	for δ := x; math.Abs(δ) > ε; {
 		δ = (r*r - x) / (2 * r)
 		r -= δ
 	}
